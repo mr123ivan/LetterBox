fb2255e2c95ec09051cb027426cf6bc4b6a1b632
mr123ivan <ivanjayadoptante@gmail.com>
2025-11-22 01:39:43 +0800

    Dockerfile Backend/frontend

--- frontend/letterbox-frontend/src/pages/CreateLetter.jsx
+++ frontend/letterbox-frontend/src/pages/CreateLetter.jsx
@@ -551,18 +551,6 @@ const handleRejectAiChat = () => {
                 </div>
                 {/* end letter content section*/}
 
-
-
-
-
-
-
-
-
-
-
-
-
                 {/* Letter Recipient with Search */}
                 <div className="relative">
                   <label htmlFor="recipient-search" className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
@@ -618,7 +606,7 @@ const handleRejectAiChat = () => {
                                 setIsDropdownOpen(false);
                               }}
                             >
-                              <span className="text-gray-900 dark:text-white">{user.userName}</span>
+                              <span className="text-gray-900 dark:text-white">{"(" + user.userId + ") " + user.userName}</span>
                             </li>
                           ))}
                       </ul>
